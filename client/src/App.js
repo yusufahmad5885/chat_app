3074e95283cbd5de601bc0d3ec666302e0eab8cb
Yusuf <yusufahmad5885@gmail.com>
2023-05-27 19:12:16 -0700

    worked on allowing two users to join a room and share messages with one another

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import io from "socket.io-client";
 import { useState } from "react";
+import Chat from "./Chat";
 
 const socket = io.connect("http://localhost:3001");
 
 function App() {
   const [userName, setUsername] = useState("");
   const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
     if (userName !== "" && room !== "") {
@@ -16,22 +18,28 @@ function App() {
 
   return (
     <div className="App">
-      <h3>Join A Chat</h3>
-      <input
-        type="text"
-        placeholder="John..."
-        onChange={(event) => {
-          setUsername(event.target.value);
-        }}
-      />
-      <input
-        type="text"
-        placeholder="Room ID..."
-        onChange={(event) => {
-          setRoom(event.target.value);
-        }}
-      />
-      <button>Join A Room</button>
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join A Chat</h3>
+          <input
+            type="text"
+            placeholder="John..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={userName} room={room} />
+      )}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -19,6 +19,11 @@ io.on("connection", (socket) => {
 
   socket.on("join_room", (data) => {
     socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("recieve_message", data);
   });
 
   socket.on("disconnect", () => {
